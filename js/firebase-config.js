d289a37a5e33f4c4ccfafb1e32570d8f37a19bc9
Farhan Asain <farhandxb.1174@gmail.com>
2025-07-27 03:28:59 +0400

    Fix: Finalize Firebase integration with correct keys and script order

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,10 +2,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyAzNcCIogv1y0RRmX5o-Ms3UdbO2WJkNHA",
   authDomain: "koush-website.firebaseapp.com",
   projectId: "koush-website",
-  storageBucket: "koush-website.firebasestorage.app",
+  storageBucket: "koush-website.appspot.com",
   messagingSenderId: "354812488121",
-  appId: "1:354812488121:web:a888e1f98b8ac01e57ae68",
-  measurementId: "G-TB566TKB1T"
+  appId: "1:354812488121:web:a888e1f98b8ac01e57ae68"
 };
 
 // Initialize Firebase
